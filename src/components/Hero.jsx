01de9eb9a0095337fa208eba5c4c3b946218a96d
Gowreesh <gowreesh369@gmail.com>
2026-06-26 14:32:10 +0530

    headline change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,9 +44,7 @@ export default function Hero() {
 
           {/* Main Headline */}
           <h1 className="text-5xl md:text-6xl lg:text-[4.5rem] font-display font-bold tracking-tight mb-8 leading-[1.05]">
-            <span className="text-text">The intelligent management layer for</span>
-            <br />
-            <span className="gradient-text-nixo">forward deployed teams</span>
+            <span className="text-text">The first </span><span className="gradient-text-nixo">FDE success</span><span className="text-text"> platform</span>
           </h1>
 
           {/* Subheadline */}
